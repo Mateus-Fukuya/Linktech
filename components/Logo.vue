c4219e99a27535298fd9c7c555670c38c9244ad7
ojoquinhaa <jlpradoneiva@gmail.com>
2023-06-01 13:42:26 -0300

    v2

---
+++ components/Logo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="w-full flex justify-center">
+    <a :href="link" class="mt-2">
+      <img :src="img" alt="Logo TypeCodes" />
+    </a>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Logo",
+  props: ["img", "link"],
+};
+</script>
