b5743fb838f7b856bab92d2725f7e31895bc6cae
Mateus <mateusgcmiguel@outlook.com>
2023-06-01 22:02:58 -0300

    Projeto v1 WhatsCode, inicio da logica pra criar o gerador de links para conversas

---
+++ whatcode/src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="hello">
+    <input type="text" v-model="inputValue" />
+    <button @click="handleButtonClick">Clique Aqui</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+export {
+  data() {
+    return {
+      inputValue: ''
+    };
+  }
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
